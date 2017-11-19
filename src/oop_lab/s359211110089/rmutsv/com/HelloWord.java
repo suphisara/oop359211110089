08e1e5722e07dbb271a6ff84f61818516b2d5eba
suphisara <lookpad_17@hotmail.com>
2017-11-19 15:15:24 +0700

    Initial commit

---
+++ src/oop_lab/s359211110089/rmutsv/com/HelloWord.java
@@ -0,0 +1,8 @@
+package oop_lab.s359211110089.rmutsv.com;
+
+public class HelloWord {
+ //   create main method
+ public static void main(String[] args) {
+     System.out.println("Hello World, I'am PAD");
+ }//main
+}
