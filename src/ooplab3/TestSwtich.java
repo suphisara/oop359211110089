f665b50176df98040f15638964638e529c325d48
Suphisara <lookpad_17@hotmail.com>
2017-11-29 10:37:29 +0700

    Test

--- src/ooplab3/TestSwtich.java
+++ src/ooplab3/TestSwtich.java
@@ -1,4 +1,22 @@
 package ooplab3;
 
-public class TestSwtich {
-}
+import java.util.Scanner;
+
+    public class TestSwtich {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+//        switch - case
+
+            int select;
+            System.out.println("Please enter number 1-3");
+            select = scanner.nextInt();
+            switch (select){
+                case 1: System.out.println("Your entered 1");break;
+                case 2: System.out.println("Your entered 2");break;
+                case 3: System.out.println("Your entered 3");break;
+                default: System.out.println("Number 1-3 only.");
+            }//switch
+        }//main
+    }//class
+
+
