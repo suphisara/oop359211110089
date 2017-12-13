897b444cc9cca229945378118b07b3a302102160
Suphisara <lookpad_17@hotmail.com>
2017-12-13 11:07:50 +0700

    TestArrayList

---
+++ src/ooplab5/TestArrayList.java
@@ -0,0 +1,28 @@
+package ooplab5;
+
+import java.util.ArrayList;
+
+public class TestArrayList {
+    public static void main (String[] args){
+        //ArrayList
+        ArrayList list = new ArrayList();
+        list.add("PAD");
+        list.add("Grid");
+        list.add("Boy");
+        System.out.println(list);
+        System.out.println(list.size());
+        list.remove(1);
+        System.out.println(list);
+        list.add(1,"YAYA");
+        System.out.println(list);
+        //print with for loop
+        for (int i=0;i<list.size();i++){
+            System.out.println(list.get(i));
+
+        }//for i
+        System.out.println(list.indexOf("YAYA"));
+        list.set(1,"BABY");
+        System.out.println(list);
+
+    }//main
+}//class
