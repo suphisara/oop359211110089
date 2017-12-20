bbf143770f1cab0faa298dbd49b84f883bc72b1c
Suphisara <lookpad17@hotmail.com>
2017-12-20 10:55:21 +0700

    Exercise

---
+++ src/ooplab6/myString.java
@@ -0,0 +1,19 @@
+package ooplab6;
+
+public class myString {
+    public  static void main (String[] args) {
+    String s;
+    s = new String();
+    s = "RMUTSV";
+    System.out.println(s);
+    s= "Welcome" + s;
+    System.out.println(s);
+
+
+
+
+
+
+    }//main
+
+}//class
