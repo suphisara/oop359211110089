265982461024ae2f9ac0c712440aeb06967262b5
Suphisara <lookpad_17@hotmail.com>
2018-01-31 11:29:53 +0700

    Employee

---
+++ src/ooplab8/EmployeeApp.java
@@ -0,0 +1,8 @@
+package ooplab8;
+
+public class EmployeeApp {
+    public static void main(String[] args) {
+        Employee employee = new Employee("Suphisara","LADE","29/02/1996",22,"MANAGER",50000);
+        System.out.println(employee.toString());
+    }
+}//class
